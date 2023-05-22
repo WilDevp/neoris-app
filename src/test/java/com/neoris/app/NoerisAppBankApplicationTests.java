0789829b0f9859e83caa6331d40e78b5b8929239
WilDevp <wilmar.garciava@gmail.com>
2023-05-22 17:06:33 -0500

    initial commit

---
+++ src/test/java/com/neoris/app/NoerisAppBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoris.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoerisAppBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
